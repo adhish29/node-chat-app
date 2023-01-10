510e83cec41bc723332ee24ed6550e49b6480b93
Adhish De <adhishde97@gmail.com>
2023-01-11 00:50:22 +0530

    initial socket setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 const publicPath = path.join(__dirname, "../public");
 const PORT = process.env.PORT || 3000;
@@ -11,6 +15,13 @@ app.get("/", (_, res) => {
   res.render("index");
 });
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("A new User Just Connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`listening to http://localhost:${PORT}`);
 });
